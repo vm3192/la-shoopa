6eb17e1df1cf02453d89c58776b85326cf87afc9
Viacheslav Mudryk <vm3192@icloud.com>
2022-07-01 19:22:01 +0300

    correct firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getDatabase} from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyA0AClTog61-TFU-oqJMuAxC5nzQB9hiOQ",
+  authDomain: "la-shoopa-49d4f.firebaseapp.com",
+  databaseURL: "https://la-shoopa-49d4f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "la-shoopa-49d4f",
+  storageBucket: "la-shoopa-49d4f.appspot.com",
+  messagingSenderId: "512351565031",
+  appId: "1:512351565031:web:13c7d1a5de6e5b18ce5e42"
 };
 
 
